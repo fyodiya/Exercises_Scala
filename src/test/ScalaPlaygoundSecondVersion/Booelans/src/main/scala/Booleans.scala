7693948842d06e85904de425dd7c03c198f5c413
fyodiya <elina.fjodorova@inbox.lv>
2021-08-16 19:50:17 +0300

    new project

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Booleans.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Booleans.scala
@@ -32,6 +32,47 @@ val isCold = true
   println("Valdis" != "Voldis")
 
   println(2*2 != 4) //result will be false, because 2*2=4
+  println(3 != 3) //also false
+
+  //we can use ! to toggle true to false and vice versa
+  //it's an old programming trick
+  var myToggle = true
+  print(myToggle)
+  myToggle = ! myToggle
+  println(myToggle)
+  //we can reverse it by using the same logic
+  // we can toggle only booleans, not inetegers or strings
+
+  //we can combine logic statements
+  println(true && true)
+  println(true && true && 5 == 5)
+  println(true && false)
+  //one bad apple ruins everything
+  println(true && true && true && false) // it's still gonna be false
+
+  println(10 > 7 && "Val" != "Vol")
+
+  println(true || true)
+  println(true || false)
+  println(false || true)
+  println(false || false) //only this will be false, others are true
+
+  println(false || false || false || false || true || false) //one true statement makes OR true
+  //we usually try to use 2-3 || or && at once
+
+  //it's hard to remember which || or && is more important
+  //so we use parenthesis
+  println((true || false) && (false || true))
+  //usually the order doesn't matter, but there are few exceptions
+
+  // conditionals, because we need to branch in order to perform operations
+
+
+
+
+
+
+
 
 
 
---
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Conditionals.scala
@@ -0,0 +1,8 @@
+object Conditionals extends App {
+  //conditionals let us branch our code, take one of multiple paths
+
+  val a = 5
+  if (a > 10) println("a is larger than 10")
+
+
+}
