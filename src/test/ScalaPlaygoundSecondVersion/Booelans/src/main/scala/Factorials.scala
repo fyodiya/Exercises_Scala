b4ac687c656887053572047147b8141b72fbb45a
fyodiya <elina.fjodorova@inbox.lv>
2021-08-17 21:40:13 +0300

    new project

---
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Factorials.scala
@@ -0,0 +1,26 @@
+import scala.io.StdIn.readLine
+
+object Factorials extends App {
+  //TODO write code to calculate Factorial from user entry
+
+  val number = readLine ("Please enter a whole number for which you want to get a factorial").toInt
+
+  //we could use while loop or also for loop
+
+  //factorial of 1 is 1
+  //of 2 is 1*2
+  //of 3 is 1*2*3
+  //of 4 is 1*2*3*4
+  //for larger numbers we need a larger number type
+
+  //we will need some variables to store our intermediate results
+  //and we will add a loop to multiply all the numbers together
+  //then we will simply print out the results
+
+  
+
+
+
+
+
+}
