5c6cb2958c824dff6974e0868f6ff5b5276118b4
fyodiya <elina.fjodorova@inbox.lv>
2021-08-17 19:18:29 +0300

    new project

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/WhileLoops.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/WhileLoops.scala
@@ -10,6 +10,10 @@ println("Alice did")
   i = i + 1 //increment the counter
   }
 
-  //so while loops run while the conditions in parethesis are true
-
+  //so while loops run while the conditions in parenthesis are true
+var floor = 5 //for while loops our condition has to ba variable since we're going to be changing
+  while (floor > 0) {
+    println(s"Taking the elevator now at the floor $floor")
+  floor -= 1 //which is the same as floor -1
+  }
 }
