98ae947ebcc237a31606f5f50c82d8b78cf5ea48
fyodiya <elina.fjodorova@inbox.lv>
2021-08-17 21:11:14 +0300

    new project

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/ExerciseFizzBuzz.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/ExerciseFizzBuzz.scala
@@ -3,7 +3,24 @@ object ExerciseFizzBuzz extends App {
   //for numbers from 1 to 100 included print FizzBuzz if number divided evenly by both 5 and 7
   //print FIZZ even number only divides by 9
   //BUZZ - by 7
-  // print number if number does not divide by by 5 or 7
+  // print number if number does not divide by 5 or 7
+
+for (i <- 1 until 100) {
+  if ((i % 5 == 0) && (i % 7 == 0)) {
+    println("FizzBuzz")
+  }
+   else {if (i % 9 == 0)
+    {println("Fizz")
+  }
+  else if (i % 7 == 0) {
+    println("Buzz")}
+  else println("i")
+  }
+  if (i != 100) print(",")
+
+
+}
+
 
 
 
