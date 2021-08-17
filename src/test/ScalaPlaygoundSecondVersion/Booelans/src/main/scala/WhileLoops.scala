4637f42416a044da368eea5e25a8b99f4060dc10
fyodiya <elina.fjodorova@inbox.lv>
2021-08-17 19:40:11 +0300

    new project

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/WhileLoops.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/WhileLoops.scala
@@ -1,3 +1,5 @@
+import scala.io.StdIn.readLine
+
 object WhileLoops extends App {
 println("Alice did")
   println("talk")
@@ -8,6 +10,7 @@ println("Alice did")
   while (i < 10) {
     println(s"talking when i is $i")
   i = i + 1 //increment the counter
+  i+=1 //increment the counter same as i = 1+1
   }
 
   //so while loops run while the conditions in parenthesis are true
@@ -16,4 +19,34 @@ var floor = 5 //for while loops our condition has to ba variable since we're goi
     println(s"Taking the elevator now at the floor $floor")
   floor -= 1 //which is the same as floor -1
   }
+  println(s"Should be at ground floor $floor")
+
+//there might be occasions when we create forever loops
+while(true) {
+  //do something
+  //ask for some exit condition
+  //exit on that condition
+}
+
+var sum:Double = 0
+while (sum < 1000) {
+  val num = readLine("Enter any number, program will quit when sum is over 1000").toDouble
+  sum += num //same as sum = sum + num
+  println(s"New sum is $sum")
+}
+
+var userInput = ""
+var counter = 0
+while (userInput.startsWith("q")) {
+  counter += 1
+  userInput = readLine("Do you want to quit? Y/N")
+  counter += 1
+  println(s"new counter value is $counter")
+  userInput = readLine("Do you want to quit? Y/N")
+}
+
+
+
+
+
 }
