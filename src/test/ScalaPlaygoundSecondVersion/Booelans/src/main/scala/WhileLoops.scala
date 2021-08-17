0471d9ab8373e4a8ca3aae8103ea615fc75c3a00
fyodiya <elina.fjodorova@inbox.lv>
2021-08-17 19:12:10 +0300

    new project

---
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/WhileLoops.scala
@@ -0,0 +1,13 @@
+object WhileLoops extends App {
+println("Alice did")
+  println("talk")
+  println("talk")
+  println("talk")
+
+  var i = 0
+  while (i < 10) {
+    println(s"talking when i is $i")}
+
+  //so while loops run while the conditions in parethesis are true
+
+}
