c5752ee572cfde370a4efb1c8ebf84a6133f613e
fyodiya <elina.fjodorova@inbox.lv>
2021-08-19 21:21:56 +0300

    exercises

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population.scala
@@ -12,5 +12,31 @@ object Population extends App {
   def cityPopulation(p0: Int, percentage: Int, delta : Int, targetPopulation: Int): Int = {
     9000 //FIXME
   }
+  //we want to return the year city will reach targetPopulation
+  //or we return -1 if the city will NEVER reach the population
+  //it is a little bit tricky because we do not want to use return statements
+  //we want to return the last line only
+  //so you probably want to use some variable to hold the result
+  //you will need a loop - while probably
+  //you will need some if else statements
+  //if we write comments /**
+  //those are so called ScalaDoc style comments and they can be used by automatic tools to create documentation
 
+  // */
+
+  /**
+   * getCityYear
+   * @param p0 - original City population
+   * @param percentage - yearly growth rate in percentages
+   * @param delta - how many people immigrate(+)/emigrate to the city each year
+   * @param targetPopulation - population we want to reach
+   * @return number of years to reach , -1 if not reachable
+   */
+  def getCityYear(p0: Int, percentage: Double, delta: Int, targetPopulation: Int):Int = {
+    9000 //FIXME
+  }
+
+  println(getCityYear(1000,2,50,1200)) // should print 3
+  println(getCityYear(1000,2,-50,1200)) // should print -1
+  println(getCityYear(1500000,2.5,10000,2000000)) // should print 10
 }
