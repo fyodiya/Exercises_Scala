d2b1cb2a368dc3f76edafff7aa0e8cf739cde3a0
fyodiya <elina.fjodorova@inbox.lv>
2021-08-23 13:39:32 +0300

    exercises

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population.scala
@@ -1,24 +1,26 @@
-object Population extends App {
+import WhileLoops.counter
 
-  println("function to calculate city growth")
-  //TODO write a function cityPop which takes the following 4 parameters
+object Population extends App {
+  //write a function cityPop which takes the following 4 parameters
   //p0: Int how many people are in the beginning
-  //perc: Int - yearly growth rate in %
+  //percentage: Int - yearly growth rate in %
   //delta: Int - how many people emigrate/immigrate each year
   //targetPopulation: Int population we want to reach
 
-  //we want to return the year city will reach targetPopulation
-  //or we return -1 the city will NEVER reach the population
-
-  def cityPopulation(p0: Int, percentage: Int, delta : Int, targetPopulation: Int): Int = {
-    9000 //FIXME
+  println("function to calculate city growth")
+  def getCityYear (p0: Int, growthPercentage: Int, pDelta: Int, targetPopulation: Int) = {
+    val result()
   }
+
 
   //we want to return the year city will reach targetPopulation
   //or we return -1 if the city will NEVER reach the population
   //it is a little bit tricky because we do not want to use return statements
   //we want to return the last line only
   //so you probably want to use some variable to hold the result
+  while(counter < 0){
+
+  }
   //you will need a loop - while probably
   //you will need some if else statements
   //if we write comments /**
@@ -41,6 +43,7 @@ object Population extends App {
   println(getCityYear(1000,2,50,1200)) // should print 3
   println(getCityYear(1000,2,-50,1200)) // should print -1
   println(getCityYear(1500000,2.5,10000,2000000)) // should print 10
+
 
 
 
