263edff6673cc02f186b01ea887be2488c3861d2
fyodiya <elina.fjodorova@inbox.lv>
2021-08-24 12:39:58 +0300

    exercises`

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population3rdTry.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population3rdTry.scala
@@ -1,7 +1,7 @@
 object Population3rdTry extends App {
   def nextYear (p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int) = targetPopulation - (p0 + pDelta + percentage/100)
 
-  def getCityYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int) = {
+  def getCityYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int): Unit = {
     var yearCounter = 0
     var currentPopulation = nextYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int)
     var isTargetPopulationAccomplished = false
@@ -19,9 +19,10 @@ object Population3rdTry extends App {
        yearCounter += 1
        println(s"After $yearCounter years the city population will reach target population")
      }
-  }
 
+  }
   println(getCityYear(1000, 2, 50, 1200)) // should print 3
   println(getCityYear(1000, 2, -50, 1200)) // should print -1
   println(getCityYear(1500000, 2.5, 10000, 2000000)) // should print 10
+
 }
---
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population4thTry.scala
@@ -0,0 +1,45 @@
+object Population4thTry extends App {
+  object Population extends App {
+    //write a function cityPop which takes the following 4 parameters
+    //p0: Int how many people are in the beginning
+    //percentage: Int - yearly growth rate in %
+    //delta: Int - how many people emigrate/immigrate each year
+    //targetPopulation: Int population we want to reach
+
+    println("Function to calculate city growth")
+
+    def getCityYear(p0: Int, percentage: Double, pDelta: Int, targetPopulation: Int): Unit = {
+      var finalPopulation: Double = {(p0 + pDelta + p0 * percentage / 100)}.toInt
+      var years: Unit = {
+        (targetPopulation - finalPopulation) / (pDelta + percentage / 100)
+        if (finalPopulation >= targetPopulation) {
+          var resultYears = 0
+          println(s"the target population will be reached in $resultYears years")
+        }
+        else
+          println("target population hasn't been reached yet")
+        val resultYears: Unit = finalPopulation = targetPopulation + 1
+        println(s"the target population will be reached in $resultYears years")
+      }
+      println("The city will NEVER reach target population")
+    }
+
+    println(getCityYear(1000, 2, 50, 1200)) // should print 3
+    println(getCityYear(1000, 2, -50, 1200)) // should print -1
+    println(getCityYear(1500000, 2.5, 10000, 2000000)) // should print 10
+
+    //we want to return the year city will reach targetPopulation
+    //or we return -1 if the city will NEVER reach the population
+    //it is a little bit tricky because we do not want to use return statements
+    //we want to return the last line only
+    //so you probably want to use some variable to hold the result
+
+    //you will need a loop - while probably
+    //you will need some if else statements
+
+    //def getCityYear(p0: Int, percentage: Double, delta: Int, targetPopulation: Int):Int
+
+  }
+
+
+}
