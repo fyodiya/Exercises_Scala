6ddaba2dbefc1515b3374d1da6aa00e456e8436f
fyodiya <elina.fjodorova@inbox.lv>
2021-08-24 16:43:06 +0300

    exercises`

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population3rdTry.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population3rdTry.scala
@@ -1,29 +1,33 @@
 object Population3rdTry extends App {
-  def nextYear (p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int) = targetPopulation - (p0 + pDelta + percentage/100)
+//  def nextYear (p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int) = currentPopulation  (p0 + pDelta + (p0*percentage/100))
 println("function to calculate the growth of city population")
-  def getCityYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int): Unit = {
+  def getCityYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int): Int = {
     var yearCounter = 0
-    var currentPopulation = nextYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int)
+    var currentPopulation = p0
+//    var currentPopulation = nextYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int)
     var isTargetPopulationAccomplished = false
+    var difference = targetPopulation - currentPopulation
 
-    if (currentPopulation < targetPopulation) {
-      yearCounter = -2
-      isTargetPopulationAccomplished = true
-    }
-    else if (currentPopulation == targetPopulation) {
-      yearCounter = -1
-      isTargetPopulationAccomplished = true
-      println("the city will never reach its target population!")
-    }
-     while (!isTargetPopulationAccomplished && currentPopulation < targetPopulation) {
-       currentPopulation = nextYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int)
-       yearCounter += 1
-       println(s"After $yearCounter years the city population will reach target population")
+     while (!isTargetPopulationAccomplished){// } && currentPopulation <= targetPopulation) {
+//       currentPopulation = nextYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int)
+       currentPopulation = (currentPopulation + pDelta + p0*percentage/100).toInt
+println (s"$currentPopulation")
+         yearCounter += 1
+       if ((targetPopulation - currentPopulation)>difference) {
+//         yearCounter = -2
+         isTargetPopulationAccomplished = true
+         println("the city will never reach its target population!")
+       }
+       else if (currentPopulation >= targetPopulation) {
+//         yearCounter = -1
+         isTargetPopulationAccomplished = true
+         println(s"After $yearCounter years the city population will reach target population")
+       }
      }
-
+yearCounter
   }
-  println(getCityYear(1000, 2, 50, 1200)) // should print 3
-  println(getCityYear(1000, 2, -50, 1200)) // should print -1
-  println(getCityYear(1500000, 2.5, 10000, 2000000)) // should print 10
+  println(getCityYear(1000,50, 2, 1200)) // should print 3
+  println(getCityYear(1000, -50, 2, 1200)) // should print -1
+  println(getCityYear(1500000, 10000, 2.5, 2000000)) // should print 10
 
 }
