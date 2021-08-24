a8ccda85cb4639293a0f76d7292a1fe999920728
fyodiya <elina.fjodorova@inbox.lv>
2021-08-24 16:44:31 +0300

    exercises`

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population3rdTry.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/Population3rdTry.scala
@@ -1,25 +1,19 @@
 object Population3rdTry extends App {
-//  def nextYear (p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int) = currentPopulation  (p0 + pDelta + (p0*percentage/100))
 println("function to calculate the growth of city population")
   def getCityYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int): Int = {
     var yearCounter = 0
     var currentPopulation = p0
-//    var currentPopulation = nextYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int)
     var isTargetPopulationAccomplished = false
     var difference = targetPopulation - currentPopulation
-
-     while (!isTargetPopulationAccomplished){// } && currentPopulation <= targetPopulation) {
-//       currentPopulation = nextYear(p0: Int, pDelta: Double, percentage: Double, targetPopulation: Int)
+     while (!isTargetPopulationAccomplished){
        currentPopulation = (currentPopulation + pDelta + p0*percentage/100).toInt
 println (s"$currentPopulation")
          yearCounter += 1
        if ((targetPopulation - currentPopulation)>difference) {
-//         yearCounter = -2
          isTargetPopulationAccomplished = true
          println("the city will never reach its target population!")
        }
        else if (currentPopulation >= targetPopulation) {
-//         yearCounter = -1
          isTargetPopulationAccomplished = true
          println(s"After $yearCounter years the city population will reach target population")
        }
