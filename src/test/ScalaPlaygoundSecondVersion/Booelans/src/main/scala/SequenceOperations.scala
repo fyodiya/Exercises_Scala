8dfbc7f6d7a6ae036b1e3291a9a2639ad1306861
fyodiya <elina.fjodorova@inbox.lv>
2021-08-26 21:58:28 +0300

    exercises

--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/SequenceOperations.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/SequenceOperations.scala
@@ -7,14 +7,19 @@ object SequenceOperations extends App {
   val bigWords = for (word <- myWords) yield word.toUpperCase
   println(bigWords.mkString(" : : :"))
 
-  myWords.slice(3,5).mkString(",")
+  myWords.slice(3, 5).mkString(",")
 
   val numbers = (0 to 10).toArray
-  val divBy3 = numbers.filter(n => n%3 == 0)
-println(divBy3.mkString(","))
+  val divBy3 = numbers.filter(n => n % 3 == 0)
+  println(divBy3.mkString(","))
 
-  val squares = numbers.map(n => n*n)
+  val squares = numbers.map(n => n * n)
   println(squares.mkString(","))
+
+  for (number <- numbers) {
+    println(s"I can do something with $number")
+  }
+
 
 
 
