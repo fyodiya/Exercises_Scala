bc3e714941c0e77015cb408d31374975fe8260ec
fyodiya <elina.fjodorova@inbox.lv>
2021-08-30 20:08:14 +0300

    exercises

---
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/MapsExercise.scala
@@ -0,0 +1,15 @@
+import scala.io.StdIn.readLine
+
+object MapsExercise extends App {
+
+  println("Split a sentence into words")
+  print("create a mapping of words to word length")
+
+    val sentence = readLine("What is your sentence?")
+val words = sentence.split(" ")
+  //TODO
+  // val myMap which contains keys of each word and values would be integers of word length
+
+
+
+}
--- src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/MapsIntro.scala
+++ src/test/ScalaPlaygoundSecondVersion/Booelans/src/main/scala/MapsIntro.scala
@@ -1,3 +1,6 @@
+import scala.concurrent.Future.never.value
+import scala.sys.SystemProperties.headless.key
+
 object MapsIntro extends App {
 
   val myNumbers = (0 to 10).toArray
@@ -11,8 +14,75 @@ object MapsIntro extends App {
 
   val myFavourites = Map(("food", "potatoes"), ("car", "vw"), ("colour", "pink"))
   println(myFavourites.mkString(","))
+//maps require keys to be unique, but values can be delegated
+
+  val anotherMap = Map("cats" -> 5, "dogs" -> 3)
+  println(s"now I have ${anotherMap("cats")} cats ")
 
 
+  //map of integers into string
+  val numericMap = Map(180 -> "heroes", 300 -> "Spartans", 9000 -> "enough")
+  println(numericMap(300))
+
+  println(myFavourites.contains("food")) //a fast way to check if key names contain food
+
+  for ((key,value) <- myFavourites) {
+    println(s"My key: $key maps to -> $value")
+  }
+
+val greenMap = for ((key,value) <- myFavourites if (value == "green")) yield key
+println(greenMap.mkString(","))
+
+  val greenKey = (for ((key,value) <- myFavourites if (value == "green")) yield key )
+
+
+// about getting values
+  //what do we do if we're not sure?
+  //we could use .contains --- or get option
+  val foodValue = myFavourites.get("food")
+  //OPTION STRING gives us an option
+  //options are useful if we might get unusable answer (we get either something or nothing)
+  val foodValueContent = foodValue.getOrElse("no food found :(")
+  println(foodValueContent)
+
+  val badKey = myFavourites.get("not a real key")
+  val badKeyContents = badKey.getOrElse("sorry no such key")
+  println(badKey)
+  println(badKeyContents)
+
+  //so we avoid exceptions that are not liked by Scala
+
+val mySeq = Seq(("one", 10), ("two", 20), ("three", 30))
+  val myMap = mySeq.toMap
+  println(mySeq)
+  println(myMap); for(((key,value), index) <- myMap.zipWithIndex) println(s"Item No.$index with $key -> $value")
+        println(myMap.getOrElse("some non existent key", "no key found, sorry"))
+
+  val myBiggerMap = myMap + ("four" -> 404)
+  println(myBiggerMap)
+  //we can print it out like this, without making a string
+
+  val mergeMap = myMap ++ anotherMap
+  println(mergeMap)
+
+  var x = Map("AL" -> "Alabama")
+  //add one element
+
+  x += ("AK" -> "Alaska")
+  println(mergeMap)
+
+  //MUTABLE MAPS
+  val muteMap = scala.collection.mutable.Map("cats" -> 5, "dogs" -> 3, "kids" -> 3)
+  println(muteMap)
+  muteMap("birds") = 452
+ //so we can add keys to existing mutable maps
+ println(muteMap)
+
+ // I can create a map out of an array through yield
+ //val squareMap = for (n <- myNumbers) yield n*n
+  //or I could go other way
+val squareMap = (for (n <- myNumbers) yield (s"Number $n squared", n*n)).toMap
+  println(squareMap)
 
 
 }
