b2ec79a8a5259b9f03c5b3cf01ce6d77bff1e433
fyodiya <elina.fjodorova@inbox.lv>
2021-09-12 21:15:29 +0300

    exercises with reading multiple values

--- src/main/scala/com/github/fyodiya/ReadingMultipleValues.scala
+++ src/main/scala/com/github/fyodiya/ReadingMultipleValues.scala
@@ -5,25 +5,18 @@ object ReadingMultipleValues extends App {
   val userInput = readLine("Enter Your first name age and eye color separated by single space ")
   //we would usually use this with strange user input coming from elsewhere
 
-  //the return is a tuple of 3 values
+  //we return a tuple of 3 values
   def parseUserInput(userInput: String): (String, Int, String) = {
     //if we expect dirty input we can clean it up a bit first
     //https://alvinalexander.com/scala/how-to-convert-multiple-spaces-to-single-space-regex/
-
     val trimmedInput = userInput.replaceAll(" +", " ") //so we replace one or more whitespace with single
-    //TODO do full regex match for name age as number and eye color as string
+    //TODO do full regex match for name ageasnumber and eyecolor as string
     trimmedInput match {
       case s"$name $age $eyeColor" => (name, age.toInt, eyeColor)
-      case s"$name" => return (name, 0, "unknown") //there were no spaces
-      case _ => return ("noName", 0, "unknown")
+      case s"$name" => (name, 0, "unknown") //there were no spaces
+      case _ => ("noName", 0, "unknown")
     }
-    userInput match {
-      case s"$name $age $eyeColor" => {
-        println(name, age, eyeColor) // for debugging
-        val ageFixed = if (age.length == 0) 0 else age.toInt
-        (name, ageFixed, eyeColor) //this is our return
-    }
-    ("Valdis", 46, "brown") //FIXME
+    //    ("Valdis", 46, "brown") //FIXME
   }
 
   //I unpack the tuple immediately since I do not like using tuples
@@ -32,6 +25,6 @@ object ReadingMultipleValues extends App {
 
   //alternative is more wordy
   val myTuple = parseUserInput(userInput)
-  val nameToo = myTuple._1 //I prefer to avoid using tuples directly
-
+  val nameToo = myTuple._1 //i prefer to avoid using tuples directly
+  //etc
 }
