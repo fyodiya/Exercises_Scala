9d94ec66cc4d4458113072335106e85b1526a85a
fyodiya <elina.fjodorova@inbox.lv>
2021-09-20 20:14:14 +0300

    reading CSV files

---
+++ src/main/scala/com/github/fyodiya/ReadingSCV.scala
@@ -0,0 +1,36 @@
+package com.github.fyodiya
+
+import SeqSpeedTest.arr
+
+object ReadingSCV extends App {
+  val srcFile = "src/resources/csv/veggie_prices_21.csv"
+  val lines = Utilities.getLinesFromFile(srcFile)
+  lines.slice(0,10).foreach(println)
+//we want to get each value in a separate line, so we need to split
+  //we can use split or yield
+  val splitLines = lines.map(_.split(","))
+  splitLines.slice(0,10).foreach(line => println(line.mkString(",")))
+
+  def arrayToVeggie(src:Array[String]):Veggie = {
+    //the hard part with CSV is handling the bad cases
+    Veggie(arr(0), arr(1), arr(3), arr(4).toDouble, arr(5))
+  }
+ val veggieLines = splitLines.tail.map(line => arrayToVeggie(line))
+  //we make some structure from our data
+
+  val prices = veggieLines.map(_.price)
+  println(s"Max price is ${prices.max}")
+
+  val maxPricedItems = veggieLines.filter(_.price == prices.max)
+
+  val sortedVeggies = veggieLines.sortBy(_.price)
+  sortedVeggies.slice(0,10).foreach(println)
+
+  //if we wanted the most expensive we could reverse the array
+  //we could also slice it, but reversing it is much easier
+  println("top 10 most expensive vegetables are:")
+  sortedVeggies.reverse.slice(0,10).foreach(println)
+
+
+
+}
