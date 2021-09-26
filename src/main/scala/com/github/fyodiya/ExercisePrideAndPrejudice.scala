7238b745ac8b7da12da024f762473104c875b0ed
fyodiya <elina.fjodorova@inbox.lv>
2021-09-26 17:40:42 +0300

    exercises with parsing text

--- src/main/scala/com/github/fyodiya/ExercisePrideAndPrejudice.scala
+++ src/main/scala/com/github/fyodiya/ExercisePrideAndPrejudice.scala
@@ -1,5 +1,7 @@
 package com.github.fyodiya
 
+import scala.collection.immutable.ListMap
+
 object ExercisePrideAndPrejudice extends App {
 //download file with scala instead of by hand
   //save to src/resources
@@ -20,13 +22,42 @@ object ExercisePrideAndPrejudice extends App {
   if (!Utilities.isFileThere(destPath)) {
     println(s"No $destPath we need to download from $url")
     val text = Utilities.getTextFromWeb(url)
-    //just notice this could take a bit so this is called blocking call, you cant proceed until you are finished
+    //just notice this could take a bit
+    //so this is called blocking call
+    //meaning that you cant proceed until it is finished
     Utilities.saveText(destPath, text)
-  } //we could add else but here we will save and then load it
+  } //we could add ELSE but here we will save and then load it
 
   val lines = Utilities.getLinesFromFile(destPath) //unless something went wrong with my HardDrive or SSD or Internet I should have the source
   val text = lines.mkString("\n")
 
+  //  val years = "\\d\\d\\d\\d".r
+  //  val years = "\\d{4}".r //same as above, better for longer numbers or range of numbers to find
+  //  val years = "[0-9]{4}".r //same as above, better for longer numbers or range of numbers to find
+  //  val years = "[1-2][0-9]{3}".r //so 1000 to 2999 more realistic range :)
+  val years = "(1[0-9]{3})|(20[0-2][0-9])".r //so 1000 to 2029 more realistic range :)
 
+  val allYears = years.findAllIn(text).toSeq
+  allYears.foreach(println)
+
+  val words = text.split("\\W+")
+  words.slice(0,20).foreach(println)
+  //  val hasUpperCase = text.filter(_.isUpper).toSeq
+  //  hasUpperCase.foreach(println)
+  val rgxForWords = "[A-Z]+[a-z]+".r
+  val wordsWithCapitalLetter = rgxForWords.findAllIn(text).toSeq
+  wordsWithCapitalLetter.slice(1,100).foreach(println)
+
+  val wordCount = wordsWithCapitalLetter.groupBy(identity).mapValues(_.size).toMap
+  val orderedCount = wordCount.to(collection.immutable.SortedMap)
+  for ((key,value) <- orderedCount) {
+    println(key, value)
+  } //so this was alphabetical ...
+  //  val countTuples = for ((key,value) <- orderedCount) yield (key,value)
+
+  //https://alvinalexander.com/scala/how-to-sort-map-in-scala-key-value-sortby-sortwith/
+  val sortedCount = ListMap(wordCount.toSeq.sortBy(_._2):_*) //so we want to sort by value not key
+  sortedCount.slice(0,50).foreach(println)
+  sortedCount.toSeq.reverse.slice(0,50).foreach(println)
 
 }
