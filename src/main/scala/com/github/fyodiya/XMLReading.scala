c8001a3b18a45f5303a09b82ccd3fa1c42695487
fyodiya <elina.fjodorova@inbox.lv>
2021-09-27 20:13:16 +0300

    adding XML library

---
+++ src/main/scala/com/github/fyodiya/XMLReading.scala
@@ -0,0 +1,9 @@
+package com.github.fyodiya
+
+object XMLReading extends App {
+//create a case class for the books
+//read books from XML
+//put the books in case class, keep all teh attributes
+//print the book objects (created from case class) in ascending order of prices
+
+}
