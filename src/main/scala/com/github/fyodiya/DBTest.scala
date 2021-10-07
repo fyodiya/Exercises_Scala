df02a1fee5f215c99712f19ab258327bdde3321c
fyodiya <elina.fjodorova@inbox.lv>
2021-10-07 20:06:40 +0300

    exercises with SQL queries

--- src/main/scala/com/github/fyodiya/DBTest.scala
+++ src/main/scala/com/github/fyodiya/DBTest.scala
@@ -3,7 +3,7 @@ package com.github.fyodiya
 import scala.collection.mutable.ListBuffer
 
 object DBTest extends App {
-  val dbPath = "./src/resources/db/chinook.db"
+  val dbPath = "./src/resources/db/chinook.db.db"
   val url = "jdbc:sqlite:"+dbPath  //the prefix will change depending on your database type here we have sqlite
 
   import java.sql.DriverManager
@@ -63,4 +63,15 @@ object DBTest extends App {
   //we print ids 6,7,8 since IDs started with 1 but slicing start with 6th element (index 5)
   // but does not include 9th element(index 8)
   genreCollection.slice(5,8).foreach(println)
+
+  val newSQL =
+    s"""
+       |SELECT * FROM media_types
+       |""".stripMargin
+  val newResultSet = statement.executeQuery(newSQL)
+  while (newResultSet.next()) {
+    //if we do not know names we can just go by column index start with 1
+    print(newResultSet.getString(1))
+    println(newResultSet.getString(2))
+  }
 }
