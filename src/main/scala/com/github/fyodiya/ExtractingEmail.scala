8fb066581fbe4dfd3370e86f3e1b8f386bf4817f
fyodiya <elina.fjodorova@inbox.lv>
2021-10-07 21:57:45 +0300

    regex for finding email addresses

---
+++ src/main/scala/com/github/fyodiya/ExtractingEmail.scala
@@ -0,0 +1,25 @@
+package com.github.fyodiya
+
+object ExtractingEmail extends App {
+
+  val srcFile = "src/resources/final_project_participants.txt"
+  val lines = Utilities.getLinesFromFile(srcFile)
+  println(lines.size)
+
+  //if e-mail are "hiding", we need more powerful tool for extraction
+  //fully compliant regex for emails is supercomplicated
+
+  val emailRegex = ".* (\\s+@\\s+\\.\\s+).*".r
+  //so we match every character until space then what we want is following:
+  //(some NON whitespaces @ some NON whitespaces . some NON whitespaces)
+
+  val emailRegex(email) = lines.head
+  println(lines.head)
+
+  lines.head match {
+    case emailRegex(email) => println(s"We got email $email")
+    case _ => println(s"We got no matches with regex")
+
+  }
+
+}
