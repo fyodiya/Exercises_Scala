e94263fa7574c2dbf112122df2b55700e83761c4
fyodiya <elina.fjodorova@inbox.lv>
2021-10-11 22:04:05 +0300

    more complicated game

--- src/main/scala/com/github/fyodiya/ExerciseOct11Nim.scala
+++ src/main/scala/com/github/fyodiya/ExerciseOct11Nim.scala
@@ -17,10 +17,32 @@ package com.github.fyodiya
     val playerA = readLine("Player A what is your name?")
     val playerB = if (playerCount == 2) readLine("Player B what is your name?") else "Computer"
     //TODO add computer level setup here
+    val computerLevel = if (playerCount == 1) readLine("computer level is 1")
     var gameState = startingCount
     var isPlayerATurn = true
 
-    println(s"Player A $playerA and Player B $playerB let's play NIM!")
+    def beforeGame(): Unit = {
+      println(s"Player A $playerA and Player B $playerB let's play NIM!")
+    }
+
+    def mainGameLoop(): Unit = {
+  while (gameState > gameEndCondition) {
+    val currentPlayer = if (isPlayerATurn) playerA else playerB
+    println(s"Currently there are $gameState matches on the table")
+    val move = getMove(currentPlayer)
+    gameState -= clampMove(move, minMove, maxMove, verbose) //same as gameState = gameState - move
+    //TODO limit the number of matches taken
+    isPlayerATurn = !isPlayerATurn //toggle a trick to change booleans
+    //ask for input
+    //change game state
+  }
+}
+    def afterGame(): Unit = {
+      val winner = if (isPlayerATurn) playerA else playerB
+      val loser = if (!isPlayerATurn) playerA else playerB
+      println(s"Game has ended. " +
+        s"Congratulations, $winner! Better luck next time, $loser")
+    }
 
     def clampMove(move:Int, min:Int = 1, max:Int = 3, verbose:Boolean = false): Int = {
       if (move > max) {
@@ -34,6 +56,12 @@ package com.github.fyodiya
       }
     }
 
+    /**
+     * Calculates the next move
+     * @param playerName - player string with "Computer" signifying computer player
+     * @return items to take from game board
+     */
+
     def getMove(playerName:String):Int = {
       if (playerName == "Computer") {
         getComputerMove(gameState)
@@ -45,12 +73,18 @@ package com.github.fyodiya
     def randomComputerMove():Int = nextInt(maxMove) + minMove
 
     def smartComputerMove(gameState:Int): Int = {
-      //some strategy, based on reminder
+      //so turns out we need some strategy based on reminder
+      //only question is whether it is maxMove or was it maxMove + 1
+      //no matter what one person takes, the second person can always guarantee that both players take 4 matches
+      //one takes 1 -> 3  2 -> 2 3 -> 1
+      //so it certainly looks like first person should lose against a computer right?
+      //second thing is strategy for computer when it is losing(against a smart player)
+      //so we need to do match against reminder of 4
       gameState % 4 match {
-        case 0 => randomComputerMove
-        case 1 => 3
-        case 2 => 1
-        case 3 => 2
+        case 0 => 3 // so from 4 to 1 and opponent loses, works for 8, 12, 16, 20
+        case 1 => randomComputerMove() //what to do when losing? :) my suggestion is play random then
+        case 2 => 1 //so from 2 to 1 and opponent loses, so 2, 6, 10, 14,
+        case 3 => 2 //so from 3 to 1 and again opponent loses, so 3, 7, 11, 15,19 and so on
 //        case _ => 0 //this default should not happen!!
       }
     }
@@ -60,25 +94,12 @@ package com.github.fyodiya
       else if (level == 2) {
         randomComputerMove()
       } else {smartComputerMove(gameState)}
-
-      //TODO level 2  which is random move between 1 and 3
-      //TODO level 3 which is optimal strategy
     }
 
-    while (gameState > gameEndCondition) {
-      val currentPlayer = if (isPlayerATurn) playerA else playerB
-      println(s"Currently there are $gameState matches on the table")
-      val move = getMove(currentPlayer)
-      gameState -= clampMove(move, minMove, maxMove, verbose) //same as gameState = gameState - move
-      //TODO limit the number of matches taken
-      isPlayerATurn = !isPlayerATurn //toggle a trick to change booleans
-      //ask for input
-      //change game state
-    }
-
-    val winner = if (isPlayerATurn) playerA else playerB
-    val loser = if (!isPlayerATurn) playerA else playerB
-    println(s"Game Ended. Congratulations $winner!   Better luck next time $loser")
+    //to start our game
+    beforeGame()
+    mainGameLoop()
+    afterGame()
 
 
 
