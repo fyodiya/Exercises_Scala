7ef2603117ff43993706c103ddbae3626b8e8e31
fyodiya <elina.fjodorova@inbox.lv>
2021-10-11 19:29:33 +0300

    simple game creation

--- src/main/scala/com/github/fyodiya/Nim.scala
+++ src/main/scala/com/github/fyodiya/Nim.scala
@@ -11,14 +11,26 @@ object Nim extends App {
 
   //TODO migrate to class based design for some organization
   var startingCount = 21
-  val gameEndCount = 0
+  val gameEndCondition = 0
   var gameState = startingCount //gameState will keep the score
   val playerA = readLine("What is Your name?")
   val playerB = readLine("What is Your name?")
 
   var isPlayerATurn = true
-   println(s"Player A $playerA, let's play a game of NIM!")
+   println(s"Player A $playerA and player B $playerB, let's play a game of NIM!")
 
+  while (gameState > gameEndCondition) {
+    val currentPlayer = if (isPlayerATurn) playerA else playerB
+    println(s"currently there are $gameState matches on the table!")
+    val move = readLine(s"How many matches do you want to take $currentPlayer?")
+    gameState -= move //same as gameState = gameState - move
+    isPlayerATurn = !isPlayerATurn //toggle a trick to change booleans
+    //ask for input
+    //change your state
+
+  }
+
+  println("Game has ended.")
 
 
 }
