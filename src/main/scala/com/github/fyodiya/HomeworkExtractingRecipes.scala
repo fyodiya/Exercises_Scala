e6f01a696a1d85a0ac2b81e666badb58a48621f5
fyodiya <elina.fjodorova@inbox.lv>
2021-10-12 13:32:56 +0300

    homework - extracting recipe titles and ingredients from a txt file through filtering and looping

--- src/main/scala/com/github/fyodiya/HomeworkExtractingRecipes.scala
+++ src/main/scala/com/github/fyodiya/HomeworkExtractingRecipes.scala
@@ -35,146 +35,53 @@ object HomeworkExtractingRecipes extends App {
       }
     }
   }
-  //println(newList)
   newList.mkString("\n")
-//  for (line <- newList) {
-//    println(line) //after all the filtering save lines in my file
-//    sum = sum + 1
-//    println(sum)
-//  }
 
-  val filteredCookbook = newList.slice(1058, 2910)
-//  filteredCookbook.foreach(println)
-
-//  val cookbookWithParagraphs = filteredCookbook.mkString
-//  val paragraph = "(\r\r)".r
-//  val regexParagraph = "\\r\\n".r
-////  val cookbookFinalVersion = new ListBuffer[String]()
-////  val paragraphsErased = filteredCookbook.filterNot(paragraph)
-////  val cookbookFinal = (regexParagraph replaceAllIn cookbookWithParagraphs)
-//  def replacingParagraphs(target: String, replacement: String): String = {
-//    (cookbookWithParagraphs).replace(("(\r\r)"), "")
-//  }
-//  println(cookbookWithParagraphs)
+  val filteredCookbook = newList.slice(289, 2910)
 
   var rowCount1 = 0
   var rowCount2 = 0
   val customRegex = "\\b[A-Z]+(?:\\s+[A-Z]+)*\\b".r
-  var finalList = new ListBuffer[String]()
+  val finalList = new ListBuffer[String]()
 
-  def checkChar(xval : String): Unit = {
-    val charArr = xval.toCharArray()
-    var c = ListBuffer()
-    for (el <- charArr) {
-      if (el.isUpper || el == " ") {
+  def checkChar(stringForArr : String) : String = {
+    var c = ""
+    for (el <- stringForArr) {
+      if (el.isUpper || el == ' ') {
         c += el
       }
-      println(c)
     }
-    var result : Boolean = false
-    println(c.size)
-    println(charArr.size)
-    if (c.size == (charArr.size)){
-      result = true
-    } else { result = false }
+    if (c.length >= stringForArr.length) {
+      return stringForArr
+    } else {
+      return "Nav"
+    }
+}
 
-    return result
-
-  }
   for (x <- filteredCookbook) {
       rowCount1 = rowCount1 + 1
-      var checkCharx = checkChar(x)
-      if (x.contains("    ") == true) {
+      if (x.contains("    ")) {
         finalList += x
-//        println(x)
       }
-
-        if (checkCharx == true) {
-        println(x)
-        for (xx <- filteredCookbook) {
-          rowCount2 = (rowCount2 + 1)
-          var checkCharxx = checkChar(xx)
-          println(checkCharxx)
-          if ((rowCount2 == (rowCount1 + 1)) == true) {
-            if (checkCharxx == true) {
-              println("LIELIE BURTI IR")
-            } else {
-              finalList += xx
-            }
-          }
-        }
+      if (checkChar(x).toString == x.toString) {
+        finalList += x.toString
+        //for (xx <- filteredCookbook) {
+         // rowCount2 = (rowCount2 + 1)
+          //if (rowCount2 == (rowCount1 + 1)) {
+            //if (checkChar(xx).toString == x.toString) {
+              //finalList += x.toString
+            //}
+          //}
+        //}
       }
       }
     finalList.foreach(println)
 
+  val finalCookbook = finalList.toArray
 
-
-      ////  val regexParagraph = {"\\r\\n){2,}"}.r
-      ////https://alvinalexander.com/source-code/scala-get-first-paragraph-from-text-string/
-
-      //  val matchText = "INDEX TO RECIPES"
-      //  val linesNumberWithMatch = newList.zipWithIndex.filter(lineTuple => lineTuple._1.contains(matchText) )
-      //  var endingIndex = 0
-      //  for (index <- 0 until newList.length) {
-      //    if (newList(index).contains(matchText)) endingIndex = index + 1
-      //  newList.foreach(println)
-
-      //    val regexStars = "*      "
-      //   val charCode = "42"
-      //    val filteringStars = mySeq
-      //    print(filteringStars)
-
-      //  for (i <- mySeq) { //changed from mySeq to filteringStars
-      //    val rgxForWords = "^\\b[A-Z]+\\b".r
-      //    val wordsWithCapitalLetter = rgxForWords.findAllIn(i).toSeq
-      //
-      //    if (i.startsWith("    ")
-      //      //      || filteringStars.nonEmpty
-      //      || wordsWithCapitalLetter.nonEmpty
-      //      || i.startsWith("^[0-9].*")
-      //    ) {
-      //      //println(i) //after all the filtering save lines in my file
-      //      sum = sum + 1
-      //      //      println(sum)
-      //    }
-      //  }
-      //val rdd2 = rdd1.filter(x => !(x._1 contains "."))
-
-
-      //  val numRex = "^[0-9].*"
-      //  val filteredFiles = files.filter(_.getName.matches(numRex))
-      //  println(filteredFiles)
-
-      //println(mySeq)
-      //  val mySeq = Seq(text)
-      //  println(mySeq)
-
-      //def filteringIngredients(mySeq:WrappedString) = {
-      //val regexForIngredients = "\n"
-      //val ingredients = "    "
-      //for(ele <- mySeq){
-      // if(ele == "    ") {
-      //  println("Jauna rinda")
-      //}
-
-      //println(ingredients)
-
-      // filteringIngredients(mySeq)
-      // val ingredients = mySeq.filter(line => lines.startsWith("^[0-9]".r))
-      // println(ingredients)
-      //}
-      //
-      //  //alternative regex for finding all capital letters words (/\b[A-Z]+\b/g)
-      //  filteringIngredients(mySeq)
-      //  val regexForTitles = "^\\b[A-Z]+\\b".r
-      //  val titles = regexForTitles.findAllIn(text).toSeq
-      //  titles.foreach(println)
-      //  TODO more filtering for false titles meaning titles which do not have ingredients following ingredients
-
-
-      // val destinationFile = "src/resources/Cookbook.txt"
-      // Utilities.saveLines(destinationFile, Cookbook) - saveList
-      //println(wordsWithCapitalLetters.mkString("\n"))
+      val destinationFile = "src/resources/Cookbook.txt"
+      Utilities.saveLines(destinationFile, finalCookbook)
+      println(finalList.mkString("\n"))
       //  Utilities.saveLines("src/resources/Cookbook.txt", titles)
 
     }
